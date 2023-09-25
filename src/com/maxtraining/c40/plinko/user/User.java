0ffeef0b449f46afef0873bb2931a41c05614326
David Benkner <d.benkner@gmail.com>
2023-09-25 12:27:10 -0400

    created user class

---
+++ src/com/maxtraining/c40/plinko/user/User.java
@@ -0,0 +1,38 @@
+package com.maxtraining.c40.plinko.user;
+
+public class User {
+	public int id;
+	public String username;
+	public String password;
+	public double score = 1000.00;
+	public User() {}
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+}
